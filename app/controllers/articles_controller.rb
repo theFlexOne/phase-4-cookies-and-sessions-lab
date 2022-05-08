b176eab7aae82b01a6563284e7f36328dd535aa4
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-05-08 14:22:39 -0500

    complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    throw StandardError if session[:page_views] >= 4
     article = Article.find(params[:id])
     render json: article
+  rescue ActiveRecord::RecordNotFound
+    record_not_found
+  rescue StandardError
+    maximum_pageviews_reached
   end
 
   private
 
+  def maximum_pageviews_reached
+    render json: { error: "Maximum pageview limit reached" }, status: 401
+  end
+
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
 end
